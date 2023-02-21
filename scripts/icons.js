bfe8363ab66b25b1471dea10460fd169d2aad516
MiepHD <63968466+MiepHD@users.noreply.github.com>
2023-02-21 12:19:36 +0100

    Made load and list seperate pages

--- scripts/icons.js
+++ scripts/icons.js
@@ -1,15 +1,10 @@
 const { ipcRenderer } = require("electron");
 window.$ = window.jQuery = require('jquery');
 
-document.addEventListener("DOMContentLoaded", () => {
-    document.querySelector("button").addEventListener("click", () => {
-        ipcRenderer.send('getIcons', $("input").value);
-    });
-});
+const params = new Proxy(new URLSearchParams(window.location.search), { get: (searchParams, prop) => searchParams.get(prop), });
+ipcRenderer.send('getIcons', params.pname);
 
-ipcRenderer.on('allIcons', (event, message) => {
-    const icons = message;
-    console.log(message);
+ipcRenderer.on('allIcons', (event, icons) => {
     for (drawable of Object.keys(icons)) {
         $("ul").append($(`<a href=""><img src="project/icons/${drawable}.png"></a>`));
     }
