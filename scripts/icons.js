6ac563f5c99d8b03c3e7da6d71cd427797ae8605
MiepHD <63968466+MiepHD@users.noreply.github.com>
2023-02-22 18:58:03 +0100

    Added raw elements of icon.html

--- scripts/icons.js
+++ scripts/icons.js
@@ -6,7 +6,7 @@ ipcRenderer.on('allIcons', (event, data) => {
     const pname = data.projectinfo.package;
     document.title = pname;
     for (drawable of Object.keys(data.iconsdata)) {
-        $("ul").append($(`<a href=""><img src="projects/${pname}/icons/${drawable}.png"></a>`));
+        $("ul").append($(`<a href="icon.html?icon=${drawable}&pname=${pname}"><img src="projects/${pname}/icons/${drawable}.png"></a>`));
     }
     console.error(data.missingimages);
 });
