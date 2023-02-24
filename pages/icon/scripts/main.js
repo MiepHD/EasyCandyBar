b92a42d7a8e5ed0fbe0b766370175d12e20955fd
MiepHD <63968466+MiepHD@users.noreply.github.com>
2023-02-24 14:23:40 +0100

    Shows image after selection

--- pages/icon/scripts/main.js
+++ pages/icon/scripts/main.js
@@ -3,14 +3,15 @@ const { ipcRenderer } = require("electron"),
 
 document.addEventListener("DOMContentLoaded", () => {
     $$("input[name=pname]").value = params.pname;
+    $$("input[name=id]").value = params.icon;
     $$("img").src = `../../projects/${params.pname}/icons/${params.icon}.png`;
     $$("a").href = `../list/list.html?pname=${params.pname}`;
-    $$("button").addEventListener("click", () => { ipcRenderer.send("chooseImagePath")});
+    $$("button").addEventListener("click", () => { ipcRenderer.send("chooseImagePath", params.icon)});
 });
 
 ipcRenderer.send('getIcon', params.icon);
 ipcRenderer.on("Icon", (e, data) => {
-    $$("input[name=name]").value = data.title;
+    $$("input[name=title]").value = data.title;
     document.title = data.title;
     $$("input[name=description]").value = data.description;
     $$("input[name=category]").value = data.category;
@@ -18,4 +19,7 @@ ipcRenderer.on("Icon", (e, data) => {
     $$("input[name=activity]").value = data.activity;
 });
 
-ipcRenderer.on("imagePath", (e, path) => { $$("input[name=imagepath]").value = path});
+ipcRenderer.on("savedImage", () => {
+    $$("img").src = `cache/${params.icon}.png`;
+    $$("input[name=imagechanged]").checked = true;
+});
