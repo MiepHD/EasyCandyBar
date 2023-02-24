d4e273c5be977a9ff6189c3c8e7e4c2b23fface4
MiepHD <63968466+MiepHD@users.noreply.github.com>
2023-02-24 14:24:21 +0100

    back-end for icon changing

--- back-end/main.js
+++ back-end/main.js
@@ -22,8 +22,8 @@ app.on("window-all-closed", () => { if (process.platform !== "darwin") app.quit(
 
 ipcMain.on("loadProject", e => {
     console.log("requested");
-    choose.dir().then(path => {
-        const data = fs.existsSync(`${path}/project.json`) ? loadproject.existing(path) : loadproject.new(path);
+    choose.dir().then(ppath => {
+        const data = fs.existsSync(`${ppath}/project.json`) ? loadproject.existing(ppath) : loadproject.new(ppath);
         const returndata = JSON.parse(`${JSON.stringify(data)}`);
         console.log("Filtering iconsdata...");
         for (key of Object.keys(data["iconsdata"])) {
@@ -46,4 +46,24 @@ ipcMain.on("getIcon", (e, icon) => {
     console.log("Sent icon");
 });
 
-ipcMain.on("chooseImagePath", e => {choose.image().then(path => { e.reply("imagePath", path)})});
+ipcMain.on("chooseImagePath", (e, id) => {choose.image().then(ipath => {
+    if (!fs.existsSync("pages/icon/cache/")){ fs.mkdirSync("pages/icon/cache/")}
+    fs.copyFileSync(ipath, `pages/icon/cache/${id}.png`);
+    e.reply("savedImage");
+})});
+
+ipcMain.on("saveIcon", (e, data) => {
+    if (data.imagechanged) {
+        console.log("Copying new file...");
+        fs.copyFileSync(`pages/icon/cache/${data.id}.png`, `projects/${data.pname}/icons/${data.id}.png`);
+        console.log("Copied new file.\nClearing cache...");
+        fs.rmSync("pages/icon/cache/", { recursive: true, force: true });
+        console.log("Cleared cache.");
+    }
+    console.log("Changing project.json...");
+    const fulldata = JSON.parse(fs.readFileSync(`projects/${data.pname}/project.json`, "utf-8"));
+    fulldata["iconsdata"][data.id] = data["iconsdata"];
+    fs.writeFileSync(`projects/${data.pname}/project.json`, JSON.stringify(fulldata));
+    console.log("Changed project.json.");
+    e.reply("savedIcon");
+})
