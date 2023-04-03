332f64cd23449e24ee60d327e4f16c5f2151b909
MiepHD <63968466+MiepHD@users.noreply.github.com>
2023-04-03 08:56:03 +0200

    Removed stack trace from consolelog

--- pages/global/scripts/utils.js
+++ pages/global/scripts/utils.js
@@ -1,10 +1,3 @@
-const oldConsoleLog = console.log;
-console.log = (message) => {
-    const error = new Error();
-    const stack = error.stack.split('\n');
-    const lastFunction = stack[2].trim().split(' ')[1];
-    oldConsoleLog(`${lastFunction}: ${message}`);
-}
 function $$(query) {
     const result = document.querySelectorAll(query);
     switch (result.length) {
