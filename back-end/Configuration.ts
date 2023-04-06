418f1ca5789da1e245b1cf1073376d8fda6598c3
MiepHD <63968466+MiepHD@users.noreply.github.com>
2023-04-06 10:01:56 +0200

    Added attributes to Configuration (not all)

--- back-end/Configuration.ts
+++ back-end/Configuration.ts
@@ -2,5 +2,49 @@
  * Structure for an object of project's configuration
  */
 interface Configuration {
-
+    /**
+     * "auto" | "light" | "dark"
+     */
+    default_theme: string;
+    description: string;
+    base_color: string;
+    /**
+     * "card_square"
+     * "card_landscape"
+     * "landscape"
+     * "square"
+     */
+    home_image_style: string;
+    home_description: string;
+    enable_check_update: boolean;
+    /**
+     * Link to dev page
+     */
+    google_play_dev: string;
+    use_flat_card: boolean;
+    navigation_view_header_title: string;
+    enable_apply: boolean;
+    enable_icons_sort: boolean;
+    show_icon_name: boolean;
+    includes_adaptive_icons: boolean;
+    enable_icon_request: boolean;
+    enable_icon_request_limit: boolean;
+    icon_request_limit: number;
+    reset_icon_request_limit: boolean;
+    json_check_before_request: boolean;
+    enable_premium_request: boolean;
+    enable_non_mail_app_request: boolean;
+    hide_missing_app_count: boolean;
+    /**
+     * "email" | "pacific"
+     */
+    regular_request_method: string;
+    /**
+     * "email" | "pacific"
+     */
+    premium_request_method: string;
+    regular_request_email: string;
+    premium_request_email: string;
+    regular_request_email_subject: string;
+    premium_request_email_subject: string;
 }
