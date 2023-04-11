4c857bb43b9c5528378284b7fbc13127222e7b6a
MiepHD <63968466+MiepHD@users.noreply.github.com>
2023-04-11 17:21:41 +0200

    Icons list is resizeable

--- pages/list/scripts/ListLoader.ts
+++ pages/list/scripts/ListLoader.ts
@@ -3,12 +3,12 @@ class ListLoader {
     public constructor() {
         const { ipcRenderer } = require("electron");
         this.params = new URLSearchParams(window.location.search);
-
+        window.addEventListener('resize', this.setColumns.bind(this));
         ipcRenderer.on("ProjectInfo", (e: any, info: ProjectStructure) => {
-            $$("button").addEventListener("click", () => { ipcRenderer.send("openFolder", this.params.get("type")); });
-            new SidebarLoader();
+            $$("#open").addEventListener("click", () => { ipcRenderer.send("openFolder", this.params.get("type")); });
+            new SidebarLoader(info.title);
             document.title = info.title;
-            ipcRenderer.on('Icons', (event: any, data: Array<string>) => {
+            ipcRenderer.on('Icons', (e: any, data: Array<string>) => {
                 this.addIcons(info.id, data);
             });
             ipcRenderer.send('getIcons', this.params.get("type"));
@@ -16,11 +16,26 @@ class ListLoader {
         ipcRenderer.send("getProjectInfo");
     }
     /**
+     * Calculates from min- and fullwidth how many icons can fit in a row
+     * @returns Number of icons that fit in a row
+     */
+	private calcIconsPerLine(): number {
+		const minwidth = $$("#ch")?.offsetWidth * 10;
+		return Math.floor($$("body")?.clientWidth / minwidth);
+	}
+    /**
+     * Resizes the list
+     */
+    private setColumns(): void {
+        $$("ul").style.setProperty("--columns", this.calcIconsPerLine());
+    }
+    /**
      * Adds an image for every icon
      * Important: received ProjectData
      * @param data Ids of icons
      */
     private addIcons(id: string, data: Array<string>): void {
+        this.setColumns();
         const type: string | null = this.params.get("type") ? this.params.get("type") : "finished";
         $$("a").href = `../icon/icon.html?type=${type}`;
         for (const drawable of data) {
