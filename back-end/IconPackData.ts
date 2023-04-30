40c28e4350b40cdd10062816ecf96c033a340f90
miephd <miep_hd@froxot.com>
2023-04-30 10:33:47 +0200

    Added more configs

---
+++ back-end/IconPackData.ts
@@ -0,0 +1,10 @@
+interface IconPackData {
+    package: string;
+    title: string;
+    author: string;
+    authorLink: string;
+    developer_link: string;
+    description: string;
+    salt: string;
+    license: string;
+}
