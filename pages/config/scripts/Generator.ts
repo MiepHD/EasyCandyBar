330b790feedf7524c16492068abd09405471b13e
MiepHD <miep_hd@froxot.com>
2023-05-10 17:33:55 +0200

    Created generator (untested)

--- pages/config/scripts/Generator.ts
+++ pages/config/scripts/Generator.ts
@@ -1,6 +1,50 @@
 class Generator {
-	private structure: any;
-	public Generator(structure: any) {
-		this.structure = structure;
+	public static Config(structure: any, root: JQuery) {
+		for (const [k, v] of Object.entries(structure)) {
+			switch (k) {
+				default:
+					if (v["type"]) {
+						switch (v["type"]) {
+							case "radio":
+								$(`
+									<fieldset id="${k}">
+										<legend>${v["name"]}</legend>
+									</fieldset>
+								`).appendTo(root);
+								for (const [option, description] of Object.entries(v["options"])) {
+									$(`
+										<label for="${k}_${option}">${description["name"]}</label>
+										<input type="radio" name="${k}" id="${k}_${option}" />
+									`).appendTo($(`fieldset#${k}`));
+								}
+								break;
+							case "text":
+							case "color":
+							case "checkbox":
+								$(`
+									<label for="${k}">${v["name"]}</label>
+									<input type="${v["type"]}" name="${k} id="${k}" />
+								`).appendTo(root);
+								break;
+							case "fieldset":
+								$(`
+									<fieldset id="${k}>
+										<legend>${v["name"]}</legend>
+									</fieldset>
+								`).appendTo(root);
+								if (v["toggle"] == true) {
+									$(`
+										<input type="checkbox" name="${k}" id="${k}_toggle" />
+									`).appendTo($(`fieldset#${k}`));
+								}
+								this.Config(v, $(`fieldset#${k}`));
+							default:
+						}
+					}
+				case "name":
+				case "type":
+				case "toggle":
+			}
+		}
 	}
 }
